422b71206ca56d32d9e8bdf19f86c36c93c0fa76
MongCheng <mongcheng@gmail.com>
2019-11-01 06:54:14 +0000

    fix typo

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo $(PWD)/volume
-docker run --rm -d -p 27017:27017 -v $(PWD)/volume:/data/db mongo
-docker run --rm -d -p 6379:6379 redis
+echo $(pwd)/volume
+#docker run --rm -d -p 27017:27017 -v $(PWD)/volume:/data/db mongo
+#docker run --rm -d -p 6379:6379 redis
