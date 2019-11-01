54d27d5b0c772307bd1b7fffce247cb46107af34
MongCheng <mongcheng@gmail.com>
2019-11-01 06:55:16 +0000

    update

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $(pwd)/volume
-docker run --rm -d -p 27017:27017 -v $(PWD)/volume:/data/db mongo
+docker run --rm -d -p 27017:27017 -v $(pwd)/volume:/data/db mongo
 docker run --rm -d -p 6379:6379 redis
